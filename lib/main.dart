39d243d7c2ba57360778272e6c1220621a8184bd
iamagf <agf0710@gmail.com>
2022-07-05 11:18:10 -0500

    Main packages required

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+// import 'package:deposit_app/views/vouchers.dart';
 import 'package:flutter/material.dart';
 import 'views/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Deposit App',
+      locale: Locale("pt"),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', ''), // English, no country code
+        Locale('pt', ''), // Spanish, no country code
+      ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
