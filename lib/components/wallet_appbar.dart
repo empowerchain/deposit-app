d67f93aad8a7736e19c55a4bb17333262e05f728
iamagf <agf0710@gmail.com>
2022-07-11 19:17:17 -0500

    Clickable voucher button

--- lib/components/wallet_appbar.dart
+++ lib/components/wallet_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:deposit_app/views/vouchers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
@@ -19,13 +20,13 @@ class _WalletAppBarState extends State<WalletAppBar> {
           children: [
             Text(
               AppLocalizations.of(context)!.mywallet,
-              style: TextStyle(
+              style: const TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 14.0,
               ),
             ),
             const SizedBox(height: 10.0),
-            Text(
+            const Text(
               "Total \$MPWR",
               style: TextStyle(
                 color: Colors.white,
@@ -33,7 +34,7 @@ class _WalletAppBarState extends State<WalletAppBar> {
               ),
             ),
             const SizedBox(height: 5.0),
-            Text(
+            const Text(
               "300.00",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -50,44 +51,64 @@ class _WalletAppBarState extends State<WalletAppBar> {
   Widget buttonsWallet(BuildContext context) {
     return Row(
       children: [
-        buttonWidget(context, "assets/images/scanIcon.png",
-            AppLocalizations.of(context)!.send),
-        buttonWidget(context, "assets/images/qrIcon.png",
-            AppLocalizations.of(context)!.receive),
-        buttonWidget(context, "assets/images/voucherIcon.png",
-            AppLocalizations.of(context)!.voucher),
+        buttonWidget(
+          context,
+          "assets/images/scanIcon.png",
+          AppLocalizations.of(context)!.send,
+          const Text("None"),
+        ),
+        buttonWidget(
+          context,
+          "assets/images/qrIcon.png",
+          AppLocalizations.of(context)!.receive,
+          const Text("None"),
+        ),
+        buttonWidget(
+          context,
+          "assets/images/voucherIcon.png",
+          AppLocalizations.of(context)!.voucher,
+          const Vouchers(),
+        ),
       ],
     );
   }
 
-  Widget buttonWidget(
-      BuildContext context, final String asset, final String title) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(
-            color: Colors.white,
-            height: 70.0,
-            width: 70.0,
-          ),
-          Column(
-            children: [
-              ImageIcon(
-                AssetImage(asset),
-                color: const Color.fromRGBO(104, 223, 87, 100),
-              ),
-              const SizedBox(height: 10.0),
-              Text(
-                title,
-                style: const TextStyle(
-                  fontSize: 12.0,
+  Widget buttonWidget(BuildContext context, final String asset,
+      final String title, Widget nextView) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => nextView),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              color: Colors.white,
+              height: 70.0,
+              width: 70.0,
+            ),
+            Column(
+              children: [
+                ImageIcon(
+                  AssetImage(asset),
+                  color: const Color.fromRGBO(104, 223, 87, 100),
                 ),
-              ),
-            ],
-          ),
-        ],
+                const SizedBox(height: 10.0),
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 12.0,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
