22da75f04cfc030c04697190968e016dfc1320ae
iamagf <agf0710@gmail.com>
2022-07-17 23:24:33 -0500

    Temporal Dumb data class-styled

--- lib/dumb_data.dart
+++ lib/dumb_data.dart
@@ -1,56 +1,58 @@
-const data = <Map>[
-  {
-    'action': 'deposit',
-    'details': 'Deposited ZZZ',
-    'date': '2022-02-24',
-    'result': '+1 Voucher'
-  },
-  {
-    'action': 'sent',
-    'details': 'Sent',
-    'date': '2021-11-20',
-    'result': '-10 MPWR'
-  },
-  {
-    'action': 'sent',
-    'details': 'Sent',
-    'date': '2021-05-24',
-    'result': '-10 MPWR'
-  },
-  {
-    'action': 'received',
-    'details': 'Received',
-    'date': '2019-02-24',
-    'result': '+250 EMP'
-  },
-  {
-    'action': 'deposit',
-    'details': 'Deposited ZZZ',
-    'date': '2022-02-24',
-    'result': '+1 Voucher'
-  },
-  {
-    'action': 'sent',
-    'details': 'Sent',
-    'date': '2021-11-20',
-    'result': '-10 MPWR'
-  },
-  {
-    'action': 'sent',
-    'details': 'Sent',
-    'date': '2021-05-24',
-    'result': '-10 MPWR'
-  },
-  {
-    'action': 'received',
-    'details': 'Received',
-    'date': '2019-02-24',
-    'result': '+250 EMP'
-  },
-  {
-    'action': 'received',
-    'details': 'Received',
-    'date': '2015-02-24',
-    'result': '+250 EMP'
-  }
+import 'package:deposit_app/classes/transaction.dart';
+
+var data = <Transaction>[
+  Transaction(
+    'deposit',
+    'Deposited something',
+    '2022-02-24',
+    '+1 Voucher',
+  ),
+  Transaction(
+    'sent',
+    'Sent',
+    '2021-11-20',
+    '-10 MPWR',
+  ),
+  Transaction(
+    'sent',
+    'Sent',
+    '2021-05-24',
+    '-10 MPWR',
+  ),
+  Transaction(
+    'received',
+    'Received',
+    '2019-02-24',
+    '+250 EMP',
+  ),
+  Transaction(
+    'deposit',
+    'Deposited ZZZ',
+    '2022-02-24',
+    '+1 Voucher',
+  ),
+  Transaction(
+    'sent',
+    'Sent',
+    '2021-11-20',
+    '-10 MPWR',
+  ),
+  Transaction(
+    'sent',
+    'Sent',
+    '2021-05-24',
+    '-10 MPWR',
+  ),
+  Transaction(
+    'received',
+    'Received',
+    '2019-02-24',
+    '+250 EMP',
+  ),
+  Transaction(
+    'received',
+    'Received',
+    '2015-02-24',
+    '+250 EMP',
+  )
 ];
