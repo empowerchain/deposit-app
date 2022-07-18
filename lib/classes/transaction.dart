96cfb2024755e34435b4576a90383c0fc9b00c4b
iamagf <agf0710@gmail.com>
2022-07-17 23:24:13 -0500

    Classes for requests and infinite listViews

---
+++ lib/classes/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String action;
+  final String details;
+  final String date;
+  final String result;
+
+  Transaction(
+    this.action,
+    this.details,
+    this.date,
+    this.result,
+  );
+}
---
+++ lib/classes/voucher.dart
@@ -0,0 +1,8 @@
+class Transaction {
+  final String code;
+  final String name;
+  final String description;
+  final bool used;
+
+  Transaction(this.code, this.name, this.description, this.used,);
+}
