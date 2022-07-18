bdb7240adad6de3092086e7d0674d42dc24f914b
iamagf <agf0710@gmail.com>
2022-07-17 23:39:14 -0500

    Voucher Class adds image url

--- lib/classes/voucher.dart
+++ lib/classes/voucher.dart
@@ -1,8 +1,15 @@
-class Transaction {
+class Voucher {
   final String code;
   final String name;
   final String description;
+  final String image;
   final bool used;
 
-  Transaction(this.code, this.name, this.description, this.used,);
+  Voucher(
+    this.code,
+    this.name,
+    this.description,
+    this.image,
+    this.used,
+  );
 }
