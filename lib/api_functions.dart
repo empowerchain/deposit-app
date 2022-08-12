d7b96e14c72dfbd154abb5e6b9ef215fc3f228ad
iamagf <agf0710@gmail.com>
2022-08-12 12:55:07 -0500

    Functions for api updated

--- lib/api_functions.dart
+++ lib/api_functions.dart
@@ -3,27 +3,44 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<Map<String,dynamic>> getVouchers(String? token) async {
+Future<Map<String, dynamic>> getVouchers(String? token) async {
   String currentToken = "Bearer $token";
 
-  final response = await http.post(Uri.parse(
-      'https://staging-deposit-pqu2.encr.app/deposit.GetAllVouchers'), headers: {"Authorization": currentToken});
+  final response = await http.post(
+      Uri.parse('https://staging-deposit-pqu2.encr.app/deposit.GetAllVouchers'),
+      headers: {"Authorization": currentToken});
   if (response.statusCode == 200) {
-    Map<String,dynamic> content = jsonDecode(response.body);
+    Map<String, dynamic> content = jsonDecode(response.body);
     return content;
   } else {
     return {};
   }
 }
 
-Future<Map<String,dynamic>> depositClaim(String? token, ) async {
+Future<Map<String, dynamic>> depositClaim(
+  String token,
+  String publicKey,
+  String code,
+) async {
   String currentToken = "Bearer $token";
 
-  final response = await http.post(Uri.parse(
-      'https://staging-deposit-pqu2.encr.app/deposit.Claim'), headers: {"Authorization": currentToken});
-  if (response.statusCode == 200) {
-    Map<String,dynamic> content = jsonDecode(response.body);
+  final Map<String, String> header = {"Authorization": currentToken};
 
+  final Map<String, dynamic> body = {
+    "depositID": code,
+    "userPubKey": publicKey
+  };
+
+  print(header);
+  print(body);
+
+  final response = await http.post(
+      Uri.parse('https://staging-deposit-pqu2.encr.app/deposit.Claim'),
+      headers: header,
+      body: body);
+  print(response.body);
+  if (response.statusCode == 200) {
+    Map<String, dynamic> content = jsonDecode(response.body);
     return content;
   } else {
     return {};
