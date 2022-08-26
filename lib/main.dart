3ffaeccfa25d64e6def41d5cf8a6f650f3c27a40
Gjermund Bjaanes <gjermund@empower.eco>
2022-08-26 13:05:15 +0300

    Added firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:depositapp/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'views/home_page.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() => runApp(MyApp());
+import 'firebase_options.dart';
+import 'views/home_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
