d4079d2389681cbfac9e6ec0de5a4f8dbb6cf555
iamagf <agf0710@gmail.com>
2022-09-02 17:50:51 -0500

    Corrected strings for language adaption

--- lib/views/myProfile.dart
+++ lib/views/myProfile.dart
@@ -16,15 +16,15 @@ class _MyProfileState extends State<MyProfile> {
   bool notMail = false;
 
   Map<String, TextEditingController> controllers = {
-    "First Name": TextEditingController(),
-    "Last Name": TextEditingController(),
-    "Email": TextEditingController()
+    "firstName": TextEditingController(),
+    "lastName": TextEditingController(),
+    "email": TextEditingController()
   };
 
   Map<String, bool> _isEnable = {
-    "First Name": false,
-    "Last Name": false,
-    "Email": false,
+    "firstName": false,
+    "lastName": false,
+    "email": false,
   };
 
   void initState() {
@@ -36,7 +36,7 @@ class _MyProfileState extends State<MyProfile> {
     });
   }
 
-  Widget characteristic(String value, String data) {
+  Widget characteristic(String className, String value, String data) {
     return Container(
       width: MediaQuery.of(context).size.width,
       child: Row(
@@ -53,7 +53,7 @@ class _MyProfileState extends State<MyProfile> {
                 width: 150.0,
                 child: TextField(
                   controller: controllers[data],
-                  onChanged: value == "Email"
+                  onChanged: className == "email"
                       ? (String input) {
                           bool verifyMail = RegExp(
                                   r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
@@ -85,7 +85,7 @@ class _MyProfileState extends State<MyProfile> {
             width: 15,
           ),
           TextButton(
-            child: _isEnable[value]!
+            child: _isEnable[className]!
                 ? Text(
                     AppLocalizations.of(context)!.save,
                     style: TextStyle(
@@ -103,7 +103,7 @@ class _MyProfileState extends State<MyProfile> {
             onPressed: () {
               setState(
                 () {
-                  _isEnable[value] = !_isEnable[value]!;
+                  _isEnable[className] = !_isEnable[className]!;
                 },
               );
             },
@@ -132,9 +132,13 @@ class _MyProfileState extends State<MyProfile> {
         actions: [
           IconButton(
             onPressed: () {
-              String firstName = controllers["First Name"]!.text == "" ? name : controllers["First Name"]!.text;
-              String lastName = controllers["Last Name"]!.text;
-              String mail = controllers["Email"]!.text == "" ? email : controllers["Email"]!.text;
+              String firstName = controllers["firstName"]!.text == ""
+                  ? name
+                  : controllers["firstName"]!.text;
+              String lastName = controllers["lastName"]!.text;
+              String mail = controllers["Email"]!.text == ""
+                  ? email
+                  : controllers["Email"]!.text;
               Navigator.push(
                 context,
                 MaterialPageRoute(
@@ -159,9 +163,11 @@ class _MyProfileState extends State<MyProfile> {
             crossAxisAlignment:
                 CrossAxisAlignment.center, //Center Row contents vertically,
             children: [
-              characteristic(AppLocalizations.of(context)!.firstname, name),
-              characteristic(AppLocalizations.of(context)!.lastname, ""),
-              characteristic("Email", email),
+              characteristic(
+                  "firstName", AppLocalizations.of(context)!.firstname, name),
+              characteristic(
+                  "lastName", AppLocalizations.of(context)!.lastname, ""),
+              characteristic("email", "Email", email),
               notMail ? Text("sup") : SizedBox(),
             ],
           ),
