dc5f89d8ce10fc97026e8b84ac429507490f6226
iamagf <agf0710@gmail.com>
2022-09-02 18:05:21 -0500

    Easier return in under dev. view

--- lib/views/underDevelopment.dart
+++ lib/views/underDevelopment.dart
@@ -10,23 +10,26 @@ class UnderDevelopment extends StatefulWidget {
 class _UnderDevelopmentState extends State<UnderDevelopment> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.white,
-        width: double.infinity,
-        child: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Sorry!\nThis part is still under development\nPlease go back",
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.black,
-                    decoration: TextDecoration.none),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(title: const Text("Under Development",style: TextStyle(color: Colors.white),),backgroundColor: Colors.black,),
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          width: double.infinity,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  "Sorry!\nThis part is still under development\nPlease double tap the screen to go back",
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.black,
+                      decoration: TextDecoration.none),
+                ),
+              ],
+            ),
           ),
         ),
       ),
