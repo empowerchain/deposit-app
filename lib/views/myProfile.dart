d5a9d0477c69ac5fa6136810e1d1f6d6f4e15681
iamagf <agf0710@gmail.com>
2022-09-07 03:06:29 -0500

    Final version of My Profile view

--- lib/views/myProfile.dart
+++ lib/views/myProfile.dart
@@ -1,3 +1,4 @@
+// ignore: file_names
 import 'package:depositapp/classes/user_simple_preferences.dart';
 import 'package:depositapp/views/sendMyProfile.dart';
 import 'package:flutter/material.dart';
@@ -11,29 +12,62 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfileState extends State<MyProfile> {
-  String name = UserSimplePreferences.getName();
-  String email = UserSimplePreferences.getMail();
   bool notMail = false;
 
-  Map<String, TextEditingController> controllers = {
-    "firstName": TextEditingController(),
-    "lastName": TextEditingController(),
-    "email": TextEditingController()
-  };
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController email = TextEditingController();
 
-  Map<String, bool> _isEnable = {
+  Map<String, bool> isEnable = {
     "firstName": false,
     "lastName": false,
     "email": false,
   };
 
-  Widget characteristic(String className, String value, String data) {
+  Widget shareButton(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        if (notMail) {
+          return;
+        }
+        String inputFirstName = firstName.text != ""
+            ? firstName.text
+            : UserSimplePreferences.getName();
+        String inputLastName = lastName.text != "" ? lastName.text : "";
+        String inputMail =
+            email.text != "" ? email.text : UserSimplePreferences.getMail();
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SendMyProfile(
+              inputFirstName,
+              inputLastName,
+              inputMail,
+            ),
+          ),
+        );
+      },
+      child: const Padding(
+          padding: EdgeInsets.fromLTRB(20, 20, 10, 20),
+          child: Text(
+            'Send',
+            textScaleFactor: 1.2,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(103, 224, 86, 1),
+            ),
+          )),
+    );
+  }
+
+  Widget characteristic(
+      String titleForRow, String id, TextEditingController input, String data) {
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       child: Row(
         children: [
           const SizedBox(width: 20.0),
-          Expanded(child: Text(value)),
+          Expanded(child: Text(titleForRow)),
           const SizedBox(
             width: 20.0,
           ),
@@ -43,12 +77,12 @@ class _MyProfileState extends State<MyProfile> {
               SizedBox(
                 width: 150.0,
                 child: TextField(
-                  controller: controllers[data],
-                  onChanged: className == "email"
-                      ? (String input) {
+                  controller: input,
+                  onChanged: id == "email"
+                      ? (emptyParameter) {
                           bool verifyMail = RegExp(
                                   r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                              .hasMatch(input);
+                              .hasMatch(input.text);
                           if (!verifyMail) {
                             setState(() {
                               notMail = true;
@@ -67,7 +101,7 @@ class _MyProfileState extends State<MyProfile> {
                       color: Colors.grey,
                     ),
                   ),
-                  enabled: _isEnable[value],
+                  enabled: isEnable[id],
                 ),
               ),
             ],
@@ -76,7 +110,7 @@ class _MyProfileState extends State<MyProfile> {
             width: 15,
           ),
           TextButton(
-            child: _isEnable[className]!
+            child: isEnable[id]!
                 ? Text(
                     AppLocalizations.of(context)!.save,
                     style: const TextStyle(
@@ -94,10 +128,32 @@ class _MyProfileState extends State<MyProfile> {
             onPressed: () {
               setState(
                 () {
-                  _isEnable[className] = !_isEnable[className]!;
+                  print(isEnable);
+                  isEnable[id] = !isEnable[id]!;
                 },
               );
             },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget notMailMessage() {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 30.0,
+          ),
+          Text(
+            AppLocalizations.of(context)!.notmail,
+            style: const TextStyle(
+              color: Colors.red,
+              fontSize: 16.0,
+            ),
           ),
         ],
       ),
@@ -121,28 +177,7 @@ class _MyProfileState extends State<MyProfile> {
         ),
         backgroundColor: Colors.white,
         actions: [
-          IconButton(
-            onPressed: () {
-              String firstName = controllers["firstName"]!.text == ""
-                  ? name
-                  : controllers["firstName"]!.text;
-              String lastName = controllers["lastName"]!.text;
-              String mail = controllers["email"]!.text == ""
-                  ? email
-                  : controllers["email"]!.text;
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      SendMyProfile(firstName, lastName, mail),
-                ),
-              );
-            },
-            icon: const Icon(
-              Icons.send_to_mobile,
-              color: Color.fromRGBO(103, 224, 86, 1),
-            ),
-          ),
+          shareButton(context),
         ],
       ),
       body: Column(
@@ -155,11 +190,24 @@ class _MyProfileState extends State<MyProfile> {
                 CrossAxisAlignment.center, //Center Row contents vertically,
             children: [
               characteristic(
-                  "firstName", AppLocalizations.of(context)!.firstname, name),
+                AppLocalizations.of(context)!.firstname,
+                "firstName",
+                firstName,
+                UserSimplePreferences.getName(),
+              ),
               characteristic(
-                  "lastName", AppLocalizations.of(context)!.lastname, ""),
-              characteristic("email", "Email", email),
-              notMail ? Text("sup") : SizedBox(),
+                AppLocalizations.of(context)!.lastname,
+                "lastName",
+                lastName,
+                UserSimplePreferences.getLastName(),
+              ),
+              characteristic(
+                "Email",
+                "email",
+                email,
+                UserSimplePreferences.getMail(),
+              ),
+              notMail ? notMailMessage() : const SizedBox(),
             ],
           ),
         ],
